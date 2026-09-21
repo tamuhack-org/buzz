00219dae96e7db103a050a4f0fc901238957430a
Jashith Raghavendra <jashith.r1@gmail.com>
2026-09-21 11:49:36 -0500

    add http to localhost string

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     ENV: str = "development"
     LOG_LEVEL: str = "INFO"
     DISCORD_BOT_TOKEN: str
-    MENTOR_CHANNEL_ID: int #discord.py expects channel ID to be int
+    MENTOR_CHANNEL_ID: int  # discord.py expects channel ID to be int
     MENTOR_ROLE_ID: int
     HMAC_SECRET: str
-    HELPR_URL: str = "localhost:3000"
+    HELPR_URL: str = "http://localhost:3000"
 
     model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
